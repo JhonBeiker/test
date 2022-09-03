36c24fe2a8855f9b72585694f352c0b50975fddb
Jhon Beiker <jhonbeiker.ma26@gmail.com>
2022-09-03 08:19:44 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	hellomod "github.com/donvito/hellomod"
+	hellomodV2 "github.com/donvito/hellomod/v2"
+)
+func main()  {
+	hellomod.SayHello()
+	hellomodV2.SayHello("Hola mundillo ")
+}
